a8ab08cc28bb376fd9d7462ffc1009582c3f21de
hemanth1508 <kvshemanthreddy@gmail.com>
2020-05-13 20:34:11 +0530

    issue API Doc

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Issue Tracking Tool",
+  "version": "0.0.1",
+  "description": "Documentation for issue tracking tool api",
+  "title": "Issue Tracking tool APIs",
+  "url": "http://ec2-13-233-168-213.ap-south-1.compute.amazonaws.com",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-05-13T15:03:12.836Z",
+    "url": "http://apidocjs.com",
+    "version": "0.22.1"
+  }
+});
